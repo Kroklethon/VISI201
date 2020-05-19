54db5a12b203a5ce5d67bf1c6800110ff2b3aa2a
Kroklethon <61271767+Kroklethon@users.noreply.github.com>
2020-05-19 10:29:38 +0200

    Add files via upload

--- reconnaissance_chiffre.py
+++ reconnaissance_chiffre.py
@@ -34,17 +34,6 @@ def imshow(img):
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-
-# on prend des images aléatoirement
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
-
-# affichage des images
-imshow(torchvision.utils.make_grid(images))	
-# print leur classe 
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-
 ### Création du réseau ### 
 class Net(nn.Module):
     def __init__(self): # initialisation 
